a0b175f066cc5b646687959e578f5021186e9104
mizso42 <jnzsmkls@gmail.com>
2021-10-09 12:57:49 +0200

    login and register methods

--- src/main/java/application/module/Controller.java
+++ src/main/java/application/module/Controller.java
@@ -3,21 +3,51 @@ package application.module;
 import application.database.DBEngine;
 import application.model.User;
 import application.util.Encryption;
+import application.util.UserIO;
 
 public class Controller {
 
-    private static User currentUser;
-    private static DBEngine engine = new DBEngine();
+    private User currentUser;
+    private DBEngine engine = new DBEngine();
 
-    public static void logIn(String userName, String pwd) {
-        currentUser = engine.auth(userName, Encryption.hashEncrypt(pwd));
+    public void logIn() {
+        do {
+            String userName = UserIO.getString("Enter username:");
+            String pwdHash = Encryption.hashEncrypt(UserIO.getString("Enter password:"));
+            currentUser = engine.auth(userName, pwdHash);
+
+            if (currentUser.getId() <= 0)
+                UserIO.error(currentUser.getUserName());
+
+        } while (currentUser.getId() <= 0);
     }
 
-    public static void logOut() {
+    public void logOut() {
         currentUser = null;
     }
 
-    public static void createNewBlog() {
+    public void register() {
+        String userName = UserIO.getString("Enter username:");
+        //TODO check userName against DB
+        String emilAddress = UserIO.getString("Enter e-mail address:");
+        //TODO check emilAddress against DB
+        boolean isMatch;
+        do {
+            String pwd;
+            boolean isStrong;
+            do {
+                pwd = UserIO.getString("Enter password:");
+                isStrong = pwd.length() > 7;
+                //TODO proper check pwd strength
+            } while (isStrong);
+            pwd = Encryption.hashEncrypt(pwd);
+            String pwd2 = Encryption.hashEncrypt(UserIO.getString("Reenter password:"));
+            isMatch = pwd.equals(pwd2);
+        } while (!isMatch);
+
+    }
+
+    public void createNewBlog() {
 
     }
 }
